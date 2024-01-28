1ec058d10f383b5a74d5487b9be467a28e1e4407
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-01-28 10:28:58 +0100

    commit for Error Enable 2FA

---
+++ src/IdentityServerAspNetIdentity/Pages/Home/ErrorEnable2FA/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServerAspNetIdentity.Pages.Error;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
